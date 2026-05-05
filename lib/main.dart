636fb5eb359cfd28b28a331e628ed98aba5e5a95
mino_o.78 <s26010@gsm.hs.kr>
2026-05-05 22:24:43 +0900

    adf

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+void main() {
+  runApp(app());
+}
+
+class app extends StatelessWidget {
+  const app({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(children: [
+                  Text('hey, Selena',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight:  FontWeight.w600
+                  ),
+                ),
+                  Text('Welcome back',
+                  style: TextStyle(
+                    color: Colors.white, 
+                  ),
+                )
+                ],)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
